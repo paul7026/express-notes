88ceab20d219f388b0f13c2655d8184e1ef1d789
paul7026 <paul702600@gmail.com>
2022-12-22 01:24:51 +0700

    deleted  unnecessary get

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ app.use(cors());
 
 app.use(express.static("build"));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
-
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
